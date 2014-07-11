08540acadece2f75a86dd6568da8c2de4c251522
Jasen Lew <jasen.lew@gmail.com>
2014-07-11 14:44:50 -0700

    Add ability to add task

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,40 @@
-angular.module('todo', ['ionic']);
+angular.module('todo', ['ionic'])
 
+  .controller('ToDoController', function ($scope, $ionicModal) {
+//    $scope.tasks = [
+//      { title: 'Collect coins' },
+//      { title: 'Eat mushrooms' },
+//      { title: 'Wash the car' },
+//      { title: 'Go with Rong to the symphony' },
+//      { title: 'Build a robot with Jaden' },
+//      { title: 'Just be amazing'}
+//    ];
+    $scope.tasks = [];
 
-//// Ionic Starter App
-//
-//// angular.module is a global place for creating, registering and retrieving Angular modules
-//// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-//// the 2nd parameter is an array of 'requires'
-//angular.module('starter', ['ionic'])
-//
-//.run(function($ionicPlatform) {
-//  $ionicPlatform.ready(function() {
-//    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-//    // for form inputs)
-//    if(window.cordova && window.cordova.plugins.Keyboard) {
-//      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//    }
-//    if(window.StatusBar) {
-//      StatusBar.styleDefault();
-//    }
-//  });
-//})
+    // Create and load the Modal
+    $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    // Called when the form is submitted
+    $scope.createTask = function(task) {
+      $scope.tasks.push({
+        title: task.title
+      });
+      $scope.taskModal.hide();
+      task.title = "";
+    };
+
+    // Open new task modal
+    $scope.newTask = function() {
+      $scope.taskModal.show();
+    };
+
+    // Close the new task modal
+    $scope.closeNewTask = function() {
+      $scope.taskModal.hide();
+    }
+  });
